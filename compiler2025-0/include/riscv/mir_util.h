b57f80c44b15a6e9bb90b23b5e7723f8f56418d0
BiDuang <me@biduang.cn>
2025-07-30 20:00:21 +0800

    Feat: Complete Binary Operation;

--- compiler2025-0/include/riscv/mir_util.h
+++ compiler2025-0/include/riscv/mir_util.h
@@ -13,13 +13,13 @@ namespace riscv {
 
 MachineInst *loadImmI(MachineBlock *block, int imm) {
   auto inst = block->pushMInst(std::make_unique<LI>(MAKE_I32, imm));
-  return (inst);
+  return inst;
 }
 
 MachineInst *loadImmF(MachineBlock *block, int imm) {
   auto tmp = block->pushMInst(std::make_unique<LI>(MAKE_I32, imm));
   auto inst = block->pushMInst(std::make_unique<RR>(RROp::MV, MAKE_F32, (tmp)));
-  return (inst);
+  return inst;
 }
 
 MachineInst *loadImmI(MachineBlock *block, float imm) {
@@ -70,14 +70,14 @@ MachineInst *addRegRegI(MachineBlock *block, MachineInst *src0,
                         MachineInst *src1) {
   auto inst =
       block->pushMInst(std::make_unique<RRR>(RRROp::ADD, MAKE_I32, src0, src1));
-  return (inst);
+  return inst;
 }
 
 MachineInst *addRegRegF(MachineBlock *block, MachineInst *src0,
                         MachineInst *src1) {
   auto inst =
       block->pushMInst(std::make_unique<RRR>(RRROp::ADD, MAKE_F32, src0, src1));
-  return (inst);
+  return inst;
 }
 
 MachineInst *addRegImmF(MachineBlock *block, MachineInst *src, float imm) {
@@ -98,8 +98,8 @@ MachineInst *addRegImmI(MachineBlock *block, MachineInst *src, int imm) {
 
 MachineInst *divRegRegF(MachineBlock *block, MachineInst *src0,
                         MachineInst *src1) {
-  return (block->pushMInst(
-      std::make_unique<RRR>(RRROp::DIV, MAKE_I32, src0, src1)));
+  return block->pushMInst(
+      std::make_unique<RRR>(RRROp::DIV, MAKE_I32, src0, src1));
 }
 
 MachineInst *divRegRegI(MachineBlock *block, MachineInst *src0,
@@ -131,7 +131,7 @@ MachineInst *divRegImmI(MachineBlock *block, MachineInst *src, int imm) {
     // Use NEG
     auto negInst =
         block->pushMInst(std::make_unique<RR>(RROp::NEG, MAKE_F32, src));
-    return (negInst);
+    return negInst;
   }
   int div = imm;
   bool isPos = true;
@@ -174,13 +174,13 @@ MachineInst *divRegImmI(MachineBlock *block, MachineInst *src, int imm) {
         std::make_unique<RRI>(RRIOp::SRLIW, MAKE_I32, src, 31)));
     auto tmp3 = (block->pushMInst(
         std::make_unique<RRR>(RRROp::ADDW, MAKE_I32, mid2, tmp2)));
-    return (tmp1);
+    return tmp1;
   }
   auto tmp2 = (block->pushMInst(
       std::make_unique<RRI>(RRIOp::SRAIW, MAKE_I32, src, 31)));
   auto tmp3 = (block->pushMInst(
       std::make_unique<RRR>(RRROp::SUBW, MAKE_I32, mid2, tmp2)));
-  return (tmp3);
+  return tmp3;
 }
 
 MachineInst *modRegReg(MachineBlock *block, MachineInst *src0,
@@ -192,6 +192,41 @@ MachineInst *modRegReg(MachineBlock *block, MachineInst *src0,
 MachineInst *modImmReg(MachineBlock *block, int imm, MachineInst *src) {
   auto midInst = loadImmI(block, imm);
   return modRegReg(block, midInst, src);
+}
+
+MachineInst *modRegImm(MachineBlock *block, MachineInst *src, int imm) {
+  if (imm == 1)
+    return block->pushMInst(std::make_unique<RR>(RROp::MV, MAKE_I32, src));
+  else if (bitcount(imm) == 1) {
+    MachineInst *mid;
+    if (imm == 2)
+      mid = block->pushMInst(
+          std::make_unique<RRI>(RRIOp::SRLIW, MAKE_I32, src, 31));
+    else {
+      mid = block->pushMInst(
+          std::make_unique<RRI>(RRIOp::SRAIW, MAKE_I32, src, 31));
+      mid = block->pushMInst(std::make_unique<RRI>(RRIOp::SRLIW, MAKE_I32, mid,
+                                                   leadingZeros(imm) + 1));
+    }
+    mid =
+        block->pushMInst(std::make_unique<RRR>(RRROp::ADD, MAKE_I32, mid, src));
+
+    if (std::abs(imm) < 2048)
+      mid = block->pushMInst(
+          std::make_unique<RRI>(RRIOp::ANDI, MAKE_I32, mid, -imm));
+    else {
+      auto inst = loadImmI(block, -imm);
+      mid = block->pushMInst(
+          std::make_unique<RRR>(RRROp::AND, MAKE_I32, mid, inst));
+    }
+    return block->pushMInst(
+        std::make_unique<RRR>(RRROp::SUBW, MAKE_I32, src, mid));
+  }
+
+  auto mid1 = divRegImmI(block, src, imm);
+  auto mid2 = mulRegImmI(block, mid1, imm);
+  return block->pushMInst(
+      std::make_unique<RRR>(RRROp::SUBW, MAKE_I32, src, mid2));
 }
 
 MachineInst *mulRegRegF(MachineBlock *block, MachineInst *src0,
@@ -215,23 +250,23 @@ MachineInst *mulRegImmI(MachineBlock *block, MachineInst *src, int imm) {
   if (imm == 0)
     return loadImmI(block, 0);
   else if (imm == 1)
-    return (block->pushMInst(std::make_unique<RR>(RROp::MV, MAKE_I32, src)));
+    return block->pushMInst(std::make_unique<RR>(RROp::MV, MAKE_I32, src));
   else if (imm == -1)
-    return (block->pushMInst(std::make_unique<RR>(RROp::NEG, MAKE_I32, src)));
+    return block->pushMInst(std::make_unique<RR>(RROp::NEG, MAKE_I32, src));
   else if (bitcount(imm) == 1)
-    return (block->pushMInst(std::make_unique<RRI>(RRIOp::SLLIW, MAKE_I32, src,
-                                                   trailingZeros(imm))));
+    return block->pushMInst(
+        std::make_unique<RRI>(RRIOp::SLLIW, MAKE_I32, src, trailingZeros(imm)));
   else if (bitcount(imm) == 2 && imm % 2 == 1) {
     auto midInst = block->pushMInst(std::make_unique<RRI>(
         RRIOp::SLLIW, MAKE_I32, src, 31 - leadingZeros(imm)));
-    return (block->pushMInst(
-        std::make_unique<RRR>(RRROp::ADDW, MAKE_I32, (midInst), src)));
+    return block->pushMInst(
+        std::make_unique<RRR>(RRROp::ADDW, MAKE_I32, (midInst), src));
   } else if (trailingZeros(imm) == 0 &&
              leadingZeros(imm) + bitcount(imm) == 32) {
     auto midInst = block->pushMInst(std::make_unique<RRI>(
         RRIOp::SLLIW, MAKE_I32, src, 32 - leadingZeros(imm)));
-    return (block->pushMInst(
-        std::make_unique<RRR>(RRROp::SUBW, MAKE_I32, (midInst), src)));
+    return block->pushMInst(
+        std::make_unique<RRR>(RRROp::SUBW, MAKE_I32, (midInst), src));
   } else {
     auto midInst = loadImmI(block, imm);
     return mulRegRegI(block, src, midInst);
@@ -240,8 +275,8 @@ MachineInst *mulRegImmI(MachineBlock *block, MachineInst *src, int imm) {
 
 MachineInst *subRegRegF(MachineBlock *block, MachineInst *src0,
                         MachineInst *src1) {
-  return (block->pushMInst(
-      std::make_unique<RRR>(RRROp::SUB, MAKE_F32, src0, src1)));
+  return block->pushMInst(
+      std::make_unique<RRR>(RRROp::SUB, MAKE_F32, src0, src1));
 }
 
 MachineInst *subRegRegI(MachineBlock *block, MachineInst *src0,
@@ -264,20 +299,93 @@ MachineInst *subRegImmF(MachineBlock *block, MachineInst *src, float imm) {
 }
 MachineInst *subRegImmI(MachineBlock *block, MachineInst *src, int imm) {
   if (-imm >= -2048 && -imm < 2048)
-    return (block->pushMInst(
-        std::make_unique<RRI>(RRIOp::ADDI, MAKE_I32, src, -imm)));
+    return block->pushMInst(
+        std::make_unique<RRI>(RRIOp::ADDI, MAKE_I32, src, -imm));
 
   auto midInst = loadImmI(block, imm);
   return subRegRegI(block, src, midInst);
 }
 
-// TODO
 MachineInst *binImmReg(MachineBlock *block, ir::BinaryInst *binInst,
-                       ir::ConstantNumber *imm, MachineInst *src);
+                       ir::ConstantNumber *imm, MachineInst *src) {
+  switch (binInst->getOp()) {
+  case ir::BinaryOp::ADD:
+    return addRegImmI(block, src, imm->intValue());
+  case ir::BinaryOp::FADD:
+    return addRegImmF(block, src, imm->floatValue());
+  case ir::BinaryOp::SDIV:
+    return divImmRegI(block, imm->intValue(), src);
+  case ir::BinaryOp::FDIV:
+    return divImmRegF(block, imm->floatValue(), src);
+  case ir::BinaryOp::SREM:
+    return modImmReg(block, imm->intValue(), src);
+  case ir::BinaryOp::MUL:
+    return mulRegImmI(block, src, imm->intValue());
+  case ir::BinaryOp::FMUL:
+    return mulRegImmF(block, src, imm->floatValue());
+  case ir::BinaryOp::SUB:
+    return subImmRegI(block, imm->intValue(), src);
+  case ir::BinaryOp::FSUB:
+    return subImmRegF(block, imm->floatValue(), src);
+  default:
+    throw std::runtime_error("Unsupported binary operator in riscv::binImmReg");
+  }
+}
 MachineInst *binRegImm(MachineBlock *block, ir::BinaryInst *binInst,
-                       MachineInst *src, ir::ConstantNumber *imm);
+                       MachineInst *src, ir::ConstantNumber *imm) {
+  switch (binInst->getOp()) {
+  case ir::BinaryOp::ADD:
+    return addRegImmI(block, src, imm->intValue());
+  case ir::BinaryOp::FADD:
+    return addRegImmF(block, src, imm->floatValue());
+  case ir::BinaryOp::SDIV:
+    return divRegImmI(block, src, imm->intValue());
+  case ir::BinaryOp::FDIV:
+    return divRegImmF(block, src, imm->floatValue());
+  case ir::BinaryOp::SREM:
+    return modRegImm(block, src, imm->intValue());
+  case ir::BinaryOp::MUL:
+    return mulRegImmI(block, src, imm->intValue());
+  case ir::BinaryOp::FMUL:
+    return mulRegImmF(block, src, imm->floatValue());
+  case ir::BinaryOp::SUB:
+    return subRegImmI(block, src, imm->intValue());
+  case ir::BinaryOp::FSUB:
+    return subRegImmF(block, src, imm->floatValue());
+  case ir::BinaryOp::XOR: {
+    auto inst = loadImmI(block, imm->intValue());
+    return block->pushMInst(
+        std::make_unique<RRR>(RRROp::XOR, MAKE_I32, src, inst));
+  }
+  default:
+    throw std::runtime_error("Unsupported binary operator in riscv::binRegImm");
+  }
+}
 MachineInst *binRegReg(MachineBlock *block, ir::BinaryInst *binInst,
-                       MachineInst *src0, MachineInst *src1);
+                       MachineInst *src0, MachineInst *src1) {
+  switch (binInst->getOp()) {
+  case ir::BinaryOp::ADD:
+    return addRegRegI(block, src0, src1);
+  case ir::BinaryOp::FADD:
+    return addRegRegF(block, src0, src1);
+  case ir::BinaryOp::SDIV:
+    return divRegRegI(block, src0, src1);
+  case ir::BinaryOp::FDIV:
+    return divRegRegF(block, src0, src1);
+  case ir::BinaryOp::SREM:
+    return modRegReg(block, src0, src1);
+  case ir::BinaryOp::MUL:
+    return mulRegRegI(block, src0, src1);
+  case ir::BinaryOp::FMUL:
+    return mulRegRegF(block, src0, src1);
+  case ir::BinaryOp::SUB:
+    return subRegRegI(block, src0, src1);
+  case ir::BinaryOp::FSUB:
+    return subRegRegF(block, src0, src1);
+  default:
+    throw std::runtime_error("Unsupported binary operator in riscv::binRegReg");
+  }
+}
 
 } // namespace riscv
 
