db91b1155290bc774c09fa53ee04eaf54844bf36
Jiaqi Yang <jy2045@hw.ac.uk>
2025-08-18 21:23:37 +0800

    Feat: try text align with symbol in data

--- src/main.cpp
+++ src/main.cpp
@@ -23,25 +23,21 @@ void writeGlobals(std::ofstream &ofs, ir::Module *module) {
       symbolsInData.push_back(global);
     }
   }
-  if (!symbolsInBss.empty()) {
-    ofs << "\t.bss\n";
+  if ((!symbolsInBss.empty()) || (!symbolsInData.empty())) {
+    ofs << "\t.align 2\n";
+    ofs << "\t.section .data\n";
   }
   for (const auto &global : symbolsInBss) {
     const int size = static_cast<int>(global->getTypeSize() / 8);
-    ofs << "\t.align 2\n";
-    ofs << "\t.size " << global->getRawName() << ", " << std::to_string(size)
-        << '\n';
+    // ofs << "\t.size " << global->getRawName() << ", " << std::to_string(size)
+    //     << '\n';
     ofs << global->getRawName() << ":\n";
-    ofs << "\t.space " << std::to_string(size) << '\n';
-  }
-  if (!symbolsInData.empty()) {
-    ofs << "\t.data\n";
+    ofs << "\t.zero " << std::to_string(size) << '\n';
   }
   for (const auto &global : symbolsInData) {
     const int size = static_cast<int>(global->getTypeSize()) / 8;
-    ofs << "\t.align 2\n";
-    ofs << "\t.size " << global->getRawName() << ", " << std::to_string(size)
-        << '\n';
+    // ofs << "\t.size " << global->getRawName() << ", " << std::to_string(size)
+    //     << '\n';
     ofs << global->getRawName() << ":\n";
     const int num = size / 4;
     if (global->isSingle()) {
@@ -80,6 +76,7 @@ void writeGlobals(std::ofstream &ofs, ir::Module *module) {
     }
   }
 }
+
 
 int main(int argc, const char *argv[]) {
   CLI::App app{"A compiler for SysY language", "compile2025-0"};
