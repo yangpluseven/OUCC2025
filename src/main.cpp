1a79b7f69260f214302ffa10a4ce6dee49a35b4b
Jiaqi Yang <jy2045@hw.ac.uk>
2025-08-18 20:12:51 +0800

    Revert "Feat: try all in data section"
    This reverts commit 563e6c91530bb9b721be94319f2c2724a8ca459a.

--- src/main.cpp
+++ src/main.cpp
@@ -23,21 +23,25 @@ void writeGlobals(std::ofstream &ofs, ir::Module *module) {
       symbolsInData.push_back(global);
     }
   }
-  if ((!symbolsInBss.empty()) || (!symbolsInData.empty())) {
-    ofs << "\t.align 2\n";
-    ofs << "\t.section .data\n";
+  if (!symbolsInBss.empty()) {
+    ofs << "\t.bss\n";
   }
   for (const auto &global : symbolsInBss) {
     const int size = static_cast<int>(global->getTypeSize() / 8);
-    // ofs << "\t.size " << global->getRawName() << ", " << std::to_string(size)
-    //     << '\n';
+    ofs << "\t.align 2\n";
+    ofs << "\t.size " << global->getRawName() << ", " << std::to_string(size)
+        << '\n';
     ofs << global->getRawName() << ":\n";
-    ofs << "\t.zero " << std::to_string(size) << '\n';
+    ofs << "\t.space " << std::to_string(size) << '\n';
+  }
+  if (!symbolsInData.empty()) {
+    ofs << "\t.data\n";
   }
   for (const auto &global : symbolsInData) {
     const int size = static_cast<int>(global->getTypeSize()) / 8;
-    // ofs << "\t.size " << global->getRawName() << ", " << std::to_string(size)
-    //     << '\n';
+    ofs << "\t.align 2\n";
+    ofs << "\t.size " << global->getRawName() << ", " << std::to_string(size)
+        << '\n';
     ofs << global->getRawName() << ":\n";
     const int num = size / 4;
     if (global->isSingle()) {
@@ -201,7 +205,7 @@ int main(int argc, const char *argv[]) {
     regAlloc.allocate();
     passManager.runLast();
     writeGlobals(ofs, mod);
-    ofs << "\t.section .text\n";
+    ofs << "\t.text\n";
     for (const auto &mFunc : mod->getMFuncs()) {
       ofs << mFunc->str();
       ofs << "\tret\n";
